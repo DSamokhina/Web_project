2ce532220671d1bd10082c4f3f93514b955dab94
DSamokhina <prishliemail@gmail.com>
2016-09-01 18:30:27 +0300

    update

---
+++ web/js/current.js
@@ -0,0 +1,62 @@
+$(function () {
+
+    $('.transactform__switch-button').bind('click', function () {
+        $('.transactform__switch-button').toggleClass('m-active')
+    });
+
+    $.ajax({
+        url: '/web/data/accounts.json',
+        dataType: 'JSON',
+        success: function (data) {
+            var sum = 0;
+            data.forEach(function (account) {
+                $('.current__table tbody').append($('<tr><td>' + account.id + '</td><td>' + account.name + '</td><td>' + account.ammount + '</td><td>' + account.currency + '</td></tr>'))
+
+                sum += account.ammount;
+            });
+            $('[data-sum=accounts]').text(sum);
+
+            drawData(data, sum);
+        }
+    });
+});
+
+function drawData (data, sum) {
+    //http://www.highcharts.com/demo/pie-legend
+   $('.current__diagramm').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        title: {
+            text: 'Состояние счетов'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: false
+                },
+                showInLegend: true
+            }
+        },
+        series: [{
+            name: 'Процент',
+            colorByPoint: true,
+            data: data.map(function (account) {
+                return {
+                    name:account.name,
+                    y: (account.ammount / sum) * 100,
+                    sliced: true,
+                    selected: true
+                }
+            })
+        }]
+    });
+}
